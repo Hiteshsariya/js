8527461e5ccba74a3645523d83b71e1e13d043ca
Hitesh Sariya <137092341+Hiteshsariya@users.noreply.github.com>
2024-11-09 03:42:57 +0000

    1st and 2nd code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "hiteshsariya@gmail.com"
+var accountPassword = "12345"
+/*
+please do not use 'var' because causes problems in block scope {
+}
+*/
+accountCity = "jaipur"
+
+accountEmail = 'hitesh'
+accountPassword = '3333'
+accountCity = 'banglore'
+let accountState;
+
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountState]);
